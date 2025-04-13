e9df84e414687719cf52999235a7b5751a7ac4dc
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-04-14 01:48:11 +0200

    fixed rest setup and added clean before setup

--- lab2_rest/setup.sh
+++ lab2_rest/setup.sh
@@ -3,5 +3,5 @@
 mvn clean package
 target="/home/server/rest_server/"
 mkdir -vp "$target"
-cp -v target/SpringSOAP-0.0.1-SNAPSHOT.jar target/Server.jar
+cp -v target/food-rest-service-0.0.1-SNAPSHOT.jar target/Server.jar
 mv -v target/Server.jar "$target"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /home/server
+
 (cd lab1 && bash setup.sh)
 (cd lab2_soap && bash setup.sh)
 (cd lab2_rest && bash setup.sh)
