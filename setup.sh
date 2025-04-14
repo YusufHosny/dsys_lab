75d9e1de0607a3ce33a79eb2c9c936e369cafd4d
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-04-14 03:21:39 +0200

    made server nonblocking

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ rm -rf /home/server
 (cd lab2_soap && bash setup.sh)
 (cd lab2_rest && bash setup.sh)
 
-printf "#!/bin/bash\n(cd rmi_server && bash runserver.sh)\n(cd soap_server && bash runserver.sh)\n(cd rest_server && bash runserver.sh)" >> "/home/server/runserver.sh"
+printf "#!/bin/bash\n(cd rmi_server && bash runserver.sh) & \n(cd soap_server && bash runserver.sh) & \n(cd rest_server && bash runserver.sh) &" >> "/home/server/runserver.sh"
 
