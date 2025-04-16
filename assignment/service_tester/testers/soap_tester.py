48ba82e2f2de2cdffd13723991d895ff653aada4
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-04-16 11:06:35 +0200

    fixed soap test script

--- assignment/service_tester/testers/soap_tester.py
+++ assignment/service_tester/testers/soap_tester.py
@@ -8,12 +8,12 @@ PORT = 1337
 
 
 def test_soap(url: str):
-    url = urlunparse(('http', f'{url}:{PORT}', 'soap/ws/', '', '', ''))
+    url = urlunparse(('http', f'{url}:{PORT}', 'ws/', '', '', ''))
     headers = {'Content-Type': 'text/xml'}
 
     payloads = []
-    for file in os.listdir('../soap/'):
-        with open(f'../soap/{file}', 'r') as f:
+    for file in os.listdir('./soap/'):
+        with open(f'./soap/{file}', 'r') as f:
             payloads += [f.read()]
     start = time.perf_counter_ns()
     try:
